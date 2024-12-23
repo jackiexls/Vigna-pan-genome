c711b66bd941496e5b835004fd5f8ee0ba28f463
jackiexls <jackieforipa@gmail.com>
2024-12-23 15:05:10 +0800

    Update run.sh

--- Genome assembly/01.hifiasm_assembly/run.sh
+++ Genome assembly/01.hifiasm_assembly/run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 hifiasm -t 30 -o ${accession} ${accession}_hifi_reads.fasta.gz
 awk -F'\t' '{if($1=="S") print ">"$2"\n"$3}' ${accession}.bp.p_ctg.gfa >${accession}.hifiasm_contigs.fa
