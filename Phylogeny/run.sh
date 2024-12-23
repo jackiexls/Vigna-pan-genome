f2cc1ce65a800e5dfaf601cb69025d23b5483747
jackiexls <jackieforipa@gmail.com>
2024-12-23 15:52:57 +0800

    Update run.sh

--- Phylogeny/run.sh
+++ Phylogeny/run.sh
@@ -1,1 +1,12 @@
+# 1. Phylogenetic tree
+VCF2Dis -i $vcf -o raw.p_dist.mat
+neighbor <neighbor.par
+mv outtree lvdou.VCF2Dis.NJtree.nwk
+
+# 2. Population structure
+for K in {1..10}
+do
+  echo $K
+  admixture -j6 --cv lvdou_0.05_0.5.bed $K | tee log${K}.out
+done
 
